1d038effa72b3371397c0af1f8b95935cc62801d
AdrianW <adrian.wojdat@gmail.com>
2014-03-23 20:43:29 +0100

    Task #283587 dadanie szablonu plikow i gitignore ,/idea

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="utf-8">
+         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+            <title>ATD TEAM</title>
+            <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+            <link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css" />
+            <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css" />
+            <link rel="SHORTCUT ICON" href=""type="image/x-icon">
+    </head>
+
+    <body>
+    
+    </body>
+</html>
