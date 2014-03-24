4e27f7185c33879c5f75955ebfa68297b31f0a00
Wasyl <wasyl@adwmicro.pl>
2014-03-24 15:44:39 +0100

    #283643 zrobiw menu

--- section/menu.php
+++ section/menu.php
@@ -1,10 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: adrian
- * Date: 24.03.14
- * Time: 12:03
- */
-?>
+<div class="menugs"></div>
+<div class="logo"><img src="img/logoatd.png" height="56"  width="115"; ></div>
+<ul  class="menug">
+			<li id="start"><a href="javascript:jumpScroll1()">Start</a></li>
+			<li id="onas"><a href="javascript:jumpScroll2()">O nas</a></li>
+			<li id="portfolio"><a href="javascript:jumpScroll3()">Portfolio</a></li>
+			<li id="klienci"><a href="javascript:jumpScroll1()">Klienci</a></li>
+			<li id="kontakt"><a href="javascript:jumpScroll2()">Kontakt</a></li>
+			
+			</ul>
 
-<h1>menu</h1>
