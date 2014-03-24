d6e5092c6aad557f005d0a74a4272e2fa728b3f4
Adrian Wojdat <adrian.wojdat@gmail.com>
2014-03-24 12:54:32 +0100

    Task #283617 dodanie sekcji Scroll (home)

--- section/scrolls.php
+++ section/scrolls.php
@@ -7,4 +7,6 @@
  */
 ?>
 
-<h2>tutaj będzie scroll</h2>
+<h2>Scroluj stronę w dół aby dowiedziec się</br> więcej o naszej pracy</h2>
+<h4>Przewiń w dół</h4>
+<img src="img/section-scroll-img/scrollsg.png">
