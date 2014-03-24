aed3cd93bde5e13d96c00dfea3229a3be0dc444a
Adrian Wojdat <adrian.wojdat@gmail.com>
2014-03-24 18:25:42 +0100

    Task #283643 wyśrodkowanie menu

--- section/menu.php
+++ section/menu.php
@@ -1,10 +1,12 @@
 <div class="menugs"></div>
-    <div class="logo"><img src="img/section-menu-img/logoatd.png" height="50" width="100"; ></div>
+    <div class="center">
         <ul class="menug">
+            <li id="hm"><a href="http://atd.adwmicro.pl"><img src="img/section-menu-img/logoatd.png" class="logo"/></a></li>
             <li id="st"><a href="javascript:jumpScroll1()">Start</a></li>
             <li id="au"><a href="javascript:jumpScroll2()">O nas</a></li>
             <li id="pf"><a href="javascript:jumpScroll3()">Portfolio</a></li>
             <li id="cs"><a href="javascript:jumpScroll1()">Klienci</a></li>
             <li id="ct"><a href="javascript:jumpScroll2()">Kontakt</a></li>
         </ul>
+        </div>
 
