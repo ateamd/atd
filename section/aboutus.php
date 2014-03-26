79a21798a39230543d06be73938ab6c6f73460e8
Wasyl <wasyl@adwmicro.pl>
2014-03-26 17:14:13 +0100

    #283619 zrobiwem Onas

--- section/aboutus.php
+++ section/aboutus.php
@@ -1,9 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: adrian
- * Date: 24.03.14
- * Time: 14:19
- */
-?>
-<h1>About Us</h1>
+<h1>O nas</h1>
+<h2>Poznaj nasza drużynę</h2>
+
+	
+<div class="onast" > Jesteśmy grupą trzech 
+ webmasterów zajmujacych się
+projektowaniem i tworzeniem stron 
+internetowych . Karzdą stronę 
+tworzymy z pasja i 
+zaangażowaniem</div>
+
+<img src="img/Onas/beg.png" class="onasb" ></img>
+
+
+
+
