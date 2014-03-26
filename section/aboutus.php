d3c5c6275b1841f3fc002eeb3fef462bef4b1954
Adrian Wojdat <adrian.wojdat@gmail.com>
2014-03-26 20:46:28 +0100

    Task #283619 Dodanie odpowiednich warstw i poprawa pozycjonowania.

--- section/aboutus.php
+++ section/aboutus.php
@@ -3,15 +3,13 @@
 <h2>Poznaj nasza drużynę</h2>
     </div>
 
-	
-<div class="onast" > Jesteśmy grupą trzech 
- webmasterów zajmujacych się
-projektowaniem i tworzeniem stron 
-internetowych . Karzdą stronę 
-tworzymy z pasja i 
-zaangażowaniem</div>
-
-<img src="img/section-aboutus-img/background.png" class="onasb" />
+<div class="glass"><div class="onast" > Jesteśmy grupą trzech
+        webmasterów zajmujacych się
+        projektowaniem i tworzeniem stron
+        internetowych . Karzdą stronę
+        tworzymy z pasja i
+        zaangażowaniem</div></div>
+<img src="img/section-aboutus-img/foto.jpg" class="onasb" />
 
 
 
