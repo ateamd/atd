090d520e1b24a77b8b2cc853e1ba65b1bcf7c78e
Adrian Wojdat <adrian.wojdat@gmail.com>
2014-03-27 19:34:47 +0100

    naprawa jebanego gita2

--- section/portfolio.php
+++ section/portfolio.php
@@ -12,41 +12,88 @@
     <h3>To już zrobiliśmy</h3>
 </div>
 <div class="portfolio_background">
-<table>
-    <tr class="height_portfolio">
-        <td class="portfolio_border">
-            <img src="img/section-portfolio-img/demo_1.png" class="demo"/>
-        </td>
-        <td>
-            <div class="portfolio_text">
-                Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz
-                w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza
-                do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle
-                elektronicznym, pozostając praktycznie niezmienionym.
-            </div>
-        </td>
-    </tr>
-    <tr class="br_portfolio">
-        <td class="portfolio_border">
+    <table>
+        <tr class="br_portfolio">
+            <td>
 
-        </td>
-        <td>
+            </td>
+            <td class="portfolio_border">
 
-        </td>
-    </tr>
-    <tr class="height_portfolio">
-        <td class="portfolio_border">
-            <div class="portfolio_text">
-                Lorem Ipsum jest tekstem stosowanym jako przykładowy
-                wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty
-                w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki.
-                Pięć wieków później zaczął być używany przemyśle elektronicznym,
-                pozostając praktycznie niezmienionym.
-            </div>
-        </td>
-        <td>
-            <img src="img/section-portfolio-img/demo_2.png" class="demo"/>
-        </td>
-    </tr>
-</table>
-    </div>
+            </td>
+            <td>
+
+            </td>
+            <td>
+
+            </td>
+        </tr>
+        <tr class="height_portfolio">
+            <td>
+                <img src="img/section-portfolio-img/demo_1.png" class="demo"/>
+            </td>
+            <td class="portfolio_border">
+                <img src="img/section-portfolio-img/strzalka_l.png" class="portfolio_strzalki_l"/>
+
+            </td>
+            <td>
+
+            </td>
+            <td>
+                <div class="portfolio_text">
+                    Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz
+                    w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w. przez nieznanego drukarza
+                    do wypełnienia tekstem próbnej książki. Pięć wieków później zaczął być używany przemyśle
+                    elektronicznym, pozostając praktycznie niezmienionym.
+                </div>
+            </td>
+        </tr>
+        <tr class="br_portfolio">
+            <td>
+
+            </td>
+            <td class="portfolio_border">
+
+            </td>
+            <td>
+
+            </td>
+            <td>
+
+            </td>
+        </tr>
+        <tr class="height_portfolio">
+            <td>
+                <div class="portfolio_text">
+                    Lorem Ipsum jest tekstem stosowanym jako przykładowy
+                    wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty
+                    w XV w. przez nieznanego drukarza do wypełnienia tekstem próbnej książki.
+                    Pięć wieków później zaczął być używany przemyśle elektronicznym,
+                    pozostając praktycznie niezmienionym..
+                </div>
+            </td>
+            <td class="portfolio_border">
+
+            </td>
+            <td>
+                <img src="img/section-portfolio-img/strzalka_r.png" class="portfolio_strzalki_r"/>
+            </td>
+            <td>
+                <img src="img/section-portfolio-img/demo_2.png" class="demo"/>
+            </td>
+        </tr>
+        <tr class="br_portfolio">
+            <td>
+
+            </td>
+            <td class="portfolio_border">
+
+            </td>
+            <td>
+
+            </td>
+            <td>
+
+            </td>
+        </tr>
+    </table>
+</div>
