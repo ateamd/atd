1a8267996fc9ff3ba1d2adaf4234cab41d1bd9bf
Adrian Wojdat <adrian.wojdat@gmail.com>
2014-03-27 17:07:47 +0100

    Task #285398 powiekszenie sekcji i dodanie strzałek

--- section/portfolio.php
+++ section/portfolio.php
@@ -13,9 +13,30 @@
 </div>
 <div class="portfolio_background">
 <table>
-    <tr class="height_portfolio">
+    <tr class="br_portfolio">
+        <td>
+
+        </td>
         <td class="portfolio_border">
+
+        </td>
+        <td>
+
+        </td>
+        <td>
+
+        </td>
+    </tr>
+    <tr class="height_portfolio">
+        <td>
             <img src="img/section-portfolio-img/demo_1.png" class="demo"/>
+        </td>
+        <td class="portfolio_border">
+            <img src="img/section-portfolio-img/strzalka_l.png" class="portfolio_strzalki_l"/>
+
+        </td>
+        <td>
+
         </td>
         <td>
             <div class="portfolio_text">
@@ -27,7 +48,13 @@
         </td>
     </tr>
     <tr class="br_portfolio">
+        <td>
+
+        </td>
         <td class="portfolio_border">
+
+        </td>
+        <td>
 
         </td>
         <td>
@@ -35,7 +62,7 @@
         </td>
     </tr>
     <tr class="height_portfolio">
-        <td class="portfolio_border">
+        <td>
             <div class="portfolio_text">
                 Lorem Ipsum jest tekstem stosowanym jako przykładowy
                 wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty
@@ -44,8 +71,28 @@
                 pozostając praktycznie niezmienionym.
             </div>
         </td>
+        <td class="portfolio_border">
+
+        </td>
+        <td>
+            <img src="img/section-portfolio-img/strzalka_r.png" class="portfolio_strzalki_r"/>
+        </td>
         <td>
             <img src="img/section-portfolio-img/demo_2.png" class="demo"/>
+        </td>
+    </tr>
+    <tr class="br_portfolio">
+        <td>
+
+        </td>
+        <td class="portfolio_border">
+
+        </td>
+        <td>
+
+        </td>
+        <td>
+
         </td>
     </tr>
 </table>
