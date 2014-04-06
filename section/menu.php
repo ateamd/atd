4970c9780b4449ea68356afaee3c8eeae60ea71e
Wasyl <wasyl@adwmicro.pl>
2014-04-06 15:09:21 +0200

    menu jen

--- section/menu.php
+++ section/menu.php
@@ -1,22 +1,38 @@
 <div class="menugs">
     <div class="center">
+<ul class="nav">
+        <li id="hm">
+            <a href="http://atd.adwmicro.pl" ><img src="img/section-menu-img/logoatd.png" itemprop="image" class="logo"></a>
+        </li>
+        <li id="st">
+            <a href="javascript:jumpScroll1()"><?php echo $lang['home']; ?></a>
+        </li>
+        <li id="au">
+            <a href="javascript:jumpScroll2()"><?php echo $lang['about']; ?></a>
+        </li>
+        <li id="pf">
+            <a href="javascript:jumpScroll3()"><?php echo $lang['portfolio']; ?></a>
+        </li>
+        <li id="cs">
+            <a href="javascript:jumpScroll4()"><?php echo $lang['clients']; ?></a>
+        </li>
+        <li id="ct">
+            <a href="javascript:jumpScroll5()"><?php echo $lang['contact']; ?></a>
+        </li>
+       
 
-            <ul class="menug">
-            <li id="hm"><a href="http://atd.adwmicro.pl"><img src="img/section-menu-img/logoatd.png" itemprop="image" class="logo"/></a></li>
-            <li id="st"><a href="javascript:jumpScroll1()"><?php echo $lang['home']; ?></a></li>
-            <li id="au"><a href="javascript:jumpScroll2()"><?php echo $lang['about']; ?></a></li>
-            <li id="pf"><a href="javascript:jumpScroll3()"><?php echo $lang['portfolio']; ?></a></li>
-            <li id="cs"><a href="javascript:jumpScroll4()"><?php echo $lang['clients']; ?></a></li>
-            <li id="ct"><a href="javascript:jumpScroll5()"><?php echo $lang['contact']; ?></a></li>
-            <li id="lg"><a href="#"><?php echo $lang['language'] ; ?> ▼&nbsp;</a></li>
-            <?php // <li id="pl" ><a href="?lang=pl" >PL</a></li> ?>
-           <?php // <li id="eng" ><a href="?lang=ang">ENG</a></li> ?>
-        </ul>
-    </div>
+
+        <li>
+            <a href="#"><?php echo $lang['language'] ; ?> ▼&nbsp;</a>
+            <ul>
+                <li><a href="?lang=pl" >PL</a></li>
+                <li><a href="?lang=ang">ENG</a></li>
+            </ul>
+        </li>
+    </ul>
 </div>
-
-
-        
+</div>
+      
     <script type="text/javascript">
      function jumpScroll1() {
     window.scroll(0,0); 
